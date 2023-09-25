1a76298551b614520082874d3367f0f0063d29f8
ClintonYounge <clintonyounge18@gmail.com>
2023-09-25 14:02:32 -0400

    Add meal model

---
+++ app/models/meal.rb
@@ -0,0 +1,10 @@
+class Meal < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :orders, dependent: :destroy
+
+  validates :name, presence: true
+  
+  def increase_amount
+    increment!(:amount)
+  end
+end
