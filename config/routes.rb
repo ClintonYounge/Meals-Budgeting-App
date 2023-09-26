75be69885983549bd678b854f2d5b0cc22481b3b
ClintonYounge <clintonyounge18@gmail.com>
2023-09-26 17:05:10 -0400

    Add welcome controller and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root "users#index"
+  unauthenticated do
+    root to: 'welcome#index', as: :unauthenticated_root
+  end
+
+  root "meals#index"
 
   resources :users, only: [:index]
+    
+  resources :meals, only: [:index, :show, :new, :create] do
+    resources :orders, only: [:index, :show, :new, :create]
+  end
 end
