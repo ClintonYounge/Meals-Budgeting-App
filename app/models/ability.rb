40e29af1669cc9a348806d9d38f5c976610bc12d
ClintonYounge <clintonyounge18@gmail.com>
2023-09-28 04:49:04 -0400

    Create ability model file

---
+++ app/models/ability.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new # guest user (not logged in)
+
+    if user.admin?
+        can :manage, :all
+    else
+      can :read, :all
+      can :create, :all
+      can :destroy, :all
+    end
+  end
+end
