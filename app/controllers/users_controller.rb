242927dd9104f939904e71e895dee5cb9aaf0541
ClintonYounge <clintonyounge18@gmail.com>
2023-10-05 13:26:58 -0400

    Add code to respond to json

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render json: @users
+  end
+  
+  def create
+    @user = User.new(user_params)
+    
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render json: @user, status: :created }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end  
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,8 @@ class User < ApplicationRecord
   def admin?
     role == 'admin'
   end
+
+  def as_json(options = {})
+    super(only: [:id, :name, :email])
+  end
 end
